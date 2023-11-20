dbacde8d5afab976d3431eeb7471a6ed0440d7fa
Bleydson Mendonça <bleydsonanderson@hotmail.com>
2023-11-20 20:51:11 -0300

    teste de alteração

--- post.php
+++ post.php
@@ -28,7 +28,7 @@
         Ea, a inventore? Officia, quas voluptatibus facilis dolor ut eaque reiciendis, atque molestias maiores facere cum dicta itaque nemo at distinctio sunt numquam? Ex itaque adipisci officiis placeat, officia maiores.
         Quisquam laborum unde repellat eaque? Ea ipsum nostrum beatae dignissimos corrupti rem! Sit, ea. Commodi voluptatibus at aliquid rerum dicta, dignissimos sint fugiat assumenda expedita possimus vitae sequi praesentium pariatur?
         Dolor quaerat ad consequatur similique saepe nesciunt eius aliquam alias minus culpa est quae, iusto quia nobis rerum voluptatum explicabo minima tempora totam perferendis sint officiis mollitia laborum qui! Consequatur.
-        Sit numquam praesentium sunt error laboriosam, perspiciatis in voluptatum illo consectetur dicta, veniam soluta assumenda. Est dolor expedita provident impedit commodi, ipsa sapiente harum perferendis. Ratione tenetur amet molestias velit?</p>
+        Sit numquam praesentium sunt error laboriosam, perspiciatis in voluptatum illo consectetur dicta, veniam soluta assumenda. Est dolor expedita provident impedit commodi, ipsa sapiente harum perferendis. Ratione tenetur amet molestias velitt?</p>
     </div>
 
     <aside id="nav-container">
